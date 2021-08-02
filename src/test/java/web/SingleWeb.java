185915a53d35417370f491f8cbb9d0a10f46620c
nithyamani <nithya.mn999@gmail.com>
2021-08-02 16:04:08 +0530

    added failed test case

--- src/test/java/web/SingleWeb.java
+++ src/test/java/web/SingleWeb.java
@@ -17,13 +17,40 @@ class SingleWeb extends BrowserStackWebRunner {
 
         driver.get("https://www.google.com/ncr");
         WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Admiral group");
+        element.submit();
+        Thread.sleep(5000);
+        String title = driver.getTitle();
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        if(title.contains("Admiral group - Google Search")) {
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Expected Result\"}}");
+        }
+        else{
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"Unexpected Result\"}}");
+            /**Create a jira ticket in case of failure in test**/
+            JiraIntegration.createJira(sessionId);
+        }
+
+        /***Get session details***/
+        String data = GetSessionDetails.sessionData(sessionId);
+        System.out.println(data);
+    }
+
+    @Test
+    public void failedTest() throws Exception {
+        /*** Fetch Session ID***/
+        SessionId sessionId = ((RemoteWebDriver)driver).getSessionId();
+
+        driver.get("https://www.google.com/ncr");
+        WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("BrowserStack");
         element.submit();
         Thread.sleep(5000);
         String title = driver.getTitle();
 
         JavascriptExecutor jse = (JavascriptExecutor)driver;
-        if(title.contains("BrowserStack - Google Search")) {
+        if(title.contains("BrowserStack - Google Search 1")) {
             jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Expected Result\"}}");
         }
         else{
